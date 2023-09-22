c730aad9d6fec7bf506f3241bb41ed760d112e73
Arthur V. M. Dantas <me@arthurvmdantas.work>
2023-09-22 08:04:26 -0300

    customizing tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-red": "hsl(0, 100%, 67%)",
+        "orange-yellow": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
+        "cobalt-blue": "hsl(234, 85%, 45%)",
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "light-lavender": "hsl(241, 100%, 89%)",
+        "dark-grey-blue": "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        sans: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
